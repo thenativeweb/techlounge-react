b982a1e33bac40760752b65b0e5975e4dbea6a67
David Losert <david.losert@gmx.de>
2020-05-08 18:40:28 +0200

    Step1: React without Tooling

---
+++ Folge_02/app.js
@@ -0,0 +1,4 @@
+const domTarget = document.querySelector('#my-react-app');
+const myContent = React.createElement('p', {id: 'my-content'}, 'My first React Content!')
+ReactDOM.render(
+	myContent, domTarget);
