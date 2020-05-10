006e309af117b26b3e3680ece6215ce2de0daed9
David Losert <david.losert@gmx.de>
2020-05-10 13:28:10 +0200

    Step2: Adding babel and JSX

---
+++ Folge_02/src/app.jsx
@@ -0,0 +1,5 @@
+const domTarget = document.querySelector('#my-react-app');
+const myContent = React.createElement('p', {id: 'my-content'}, 'My first React Content!')
+const myJSXContent = <p id='my-content'>My second React-Content!</p>
+ReactDOM.render(
+	myJSXContent, domTarget);
