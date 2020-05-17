d0d5930c0c11bf322d18a8157781869041dcfb45
David Losert <david.losert@gmx.de>
2020-05-17 09:38:35 +0200

    Added ESLint Config and applied it to all files

--- Folge_02/src/app.js
+++ Folge_02/src/app.js
@@ -1,4 +1,3 @@
 const domTarget = document.querySelector('#my-react-app');
-const myContent = React.createElement('p', {id: 'my-content'}, 'My first React Content!')
-ReactDOM.render(
-	myContent, domTarget);
+const myContent = React.createElement('p', { id: 'my-content' }, 'My first React Content!');
+ReactDOM.render(myContent, domTarget);
--- Folge_02/src/app.jsx
+++ Folge_02/src/app.jsx
@@ -1,5 +1,4 @@
 const domTarget = document.querySelector('#my-react-app');
-const myContent = React.createElement('p', {id: 'my-content'}, 'My first React Content!')
+const myContent = React.createElement('p', { id: 'my-content' }, 'My first React Content!')
 const myJSXContent = <p id='my-content'>My second React-Content!</p>
-ReactDOM.render(
-	myJSXContent, domTarget);
+ReactDOM.render(myJSXContent, domTarget);
