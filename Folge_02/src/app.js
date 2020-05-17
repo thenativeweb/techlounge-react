290ae23636fc076fa21b47513cf16d87a3f4b5a4
David Losert <david.losert@gmx.de>
2020-05-17 17:58:35 +0200

    Fixed formatting and eslint issues

--- Folge_02/src/app.js
+++ Folge_02/src/app.js
@@ -1,3 +1,6 @@
+'use strict';
+
 const domTarget = document.querySelector('#my-react-app');
 const myContent = React.createElement('p', { id: 'my-content' }, 'My first React Content!');
+
 ReactDOM.render(myContent, domTarget);
--- Folge_02/src/app.jsx
+++ Folge_02/src/app.jsx
@@ -1,4 +1,6 @@
+'use strict';
+
 const domTarget = document.querySelector('#my-react-app');
-const myContent = React.createElement('p', { id: 'my-content' }, 'My first React Content!')
-const myJSXContent = <p id='my-content'>My second React-Content!</p>
+const myJSXContent = <p id='my-content'>My second React-Content!</p>;
+
 ReactDOM.render(myJSXContent, domTarget);
