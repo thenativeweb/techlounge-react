c1bae804629f4509a5c6f3b32fed3ad6745a0d12
David Losert <david.losert@gmx.de>
2020-05-22 12:36:50 +0200

    React folge2 setup react (#1)
    * Step1: React without Tooling
    
    * Step2: Adding babel and JSX
    
    * Removed preset-env as we acutally don't need it yet
    
    * Added ESLint Config and applied it to all files
    
    * Fixed formatting and eslint issues
    
    * Added a little readme
    
    * Added some basic comands to npm
    
    * Replaced tabs with spaces
    
    * Added React and ReactDOM as globals
    
    * Added package.json on root level to enable eslint configuration
    
    * Renamed episode folder and fixed review issues
    
    * Further extended the readme.md
    
    * Removed unused test script and made babel versions static
    
    * Also added hidden changes requested by review
    
    * Also fixed package-lock.json with new name

---
+++ 02-react-setup/src/app.jsx
@@ -0,0 +1,6 @@
+'use strict';
+
+const domTarget = document.querySelector('#my-react-app');
+const myJsxContent = <p id='my-content'>My second React content!</p>;
+
+ReactDOM.render(myJsxContent, domTarget);
