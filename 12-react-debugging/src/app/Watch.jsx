26a42b8b6fbda7d34944acca46269063ed0252c9
David Losert <david.losert@gmx.de>
2020-12-12 10:43:00 +0100

    react-12: Added bug in async code

--- 12-react-debugging/src/app/Watch.jsx
+++ 12-react-debugging/src/app/Watch.jsx
@@ -8,6 +8,7 @@ export const Watch = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTime(getCurrentTime());
+      throw new Error('Fehler in asynchroner Komponente. Dies führt zwar nicht zum App-Crash - ist aber dennoch unschön und kann negative Seiteneffekte haben. Dieser Fehler sollte also definitiv bestitigt (in diesem Fall gelöscht) werden.');
     }, 1000);
 
     return () => clearInterval(intervalId);
