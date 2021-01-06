dc3bf7d56204f045f6abc74353b315f33e5d9daa
David Losert <david.losert@gmx.de>
2021-01-06 11:53:35 +0100

    React 16 update react webpack (#17)
    * React 16: Updated React and Webpack
    
    * React 16: Switched to new JSX Compile
    
    * React 16: Added concurrently for easier app startup
    
    * React 16: Updated description and title
    
    * Update package.json
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

---
+++ 16-update-react-webpack/src/components/NumericalInput.jsx
@@ -0,0 +1,20 @@
+const allowedInputRegex = /^\d*\.?\d*$/;
+
+export const NumericalInput = ({ name, value, onChange }) => {
+  const handleInput = event => {
+    const newValue = event.target.value;
+
+    if (allowedInputRegex.test(newValue)) {
+      onChange(event);
+    }
+  };
+
+  return (
+    <input
+      type='text'
+      name={ name }
+      value={ value }
+      onChange={ handleInput }
+    />
+  );
+};
