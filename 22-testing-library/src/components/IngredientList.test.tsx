9e45020fd93e31fd5d6f41465f5d7659d3157907
David Losert <david.losert@gmx.de>
2021-06-16 14:55:54 +0200

    chore: completely switches to screen method

--- 22-testing-library/src/components/IngredientList.test.tsx
+++ 22-testing-library/src/components/IngredientList.test.tsx
@@ -1,28 +1,28 @@
 import { assert } from 'assertthat';
 import { createIngredient } from '../../fixtures/createIngredient';
 import { IngredientsList } from './IngredientsList';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 describe('IngredientList', (): void => {
-  it('given no ingredients, renders empty list.', async (): Promise<void> => {
-    const { getByLabelText } = render(<IngredientsList items={ [] } />);
+  it('given no ingredients, renders an empty list.', async (): Promise<void> => {
+    render(<IngredientsList items={ [] } />);
 
-    assert.that(getByLabelText('Zutaten')).is.not.null();
-    assert.that(getByLabelText('Zutaten').children.length).is.equalTo(0);
+    assert.that(screen.getByLabelText('Zutaten')).is.not.null();
+    assert.that(screen.getByLabelText('Zutaten').children.length).is.equalTo(0);
   });
 
-  it('given single ingredient, ,enderes the name, amount and unit as text.', async (): Promise<void> => {
+  it('given single ingredient, renderes the name, amount and unit as text.', async (): Promise<void> => {
     const ingredient = createIngredient({
       amount: 200,
       name: 'TestIngredient',
       unit: 'Liter'
     });
 
-    const { getByText } = render(
+    render(
       <IngredientsList items={ [ ingredient ] } />
     );
 
-    assert.that(getByText('200 Liter TestIngredient')).is.not.null();
+    assert.that(screen.getByText('200 Liter TestIngredient')).is.not.null();
   });
 
   it('given multiple items, renders them all.', async (): Promise<void> => {
@@ -32,12 +32,12 @@ describe('IngredientList', (): void => {
       createIngredient({ name: 'Third', amount: 3, unit: 'Stück' })
     ];
 
-    const { getByText } = render(
+    render(
       <IngredientsList items={ ingredients } />
     );
 
-    assert.that(getByText('1 Gramm First')).is.not.null();
-    assert.that(getByText('2 Liter Second')).is.not.null();
-    assert.that(getByText('3 Stück Third')).is.not.null();
+    assert.that(screen.getByText('1 Gramm First')).is.not.null();
+    assert.that(screen.getByText('2 Liter Second')).is.not.null();
+    assert.that(screen.getByText('3 Stück Third')).is.not.null();
   });
 });
