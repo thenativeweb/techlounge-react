723e5f23638ad2ed529ff7c3f33c110b8529f908
David Losert <david.losert@gmx.de>
2021-07-11 15:28:43 +0200

    chore: Adds copy of 22 folder to 24 to prepare next episode

---
+++ 24-user-events/src/app/RecipeForm/IngredientFormPart.tsx
@@ -0,0 +1,39 @@
+import { Ingredient } from '../../types/Ingredient';
+import { NumericalInput } from '../../components/NumericalInput';
+import { RecipeFormChangeEvent } from './types/RecipeFormChangeEvent';
+import { UnitInput } from '../../components/UnitInput';
+import { FunctionComponent, ReactElement } from 'react';
+
+interface IngredientFormPartProps {
+  ingredient: Ingredient;
+  onChange: (event: RecipeFormChangeEvent, name: string) => void;
+}
+
+const IngredientFormPart: FunctionComponent<IngredientFormPartProps> = ({ ingredient, onChange }): ReactElement => (
+  <div className='ingredient-form'>
+    <label>Zutat:
+      {' '}
+      <input
+        type='text'
+        value={ ingredient.name }
+        name='name'
+        onChange={ (event): void => onChange(event, ingredient.name) }
+      />
+    </label>
+    <label> Menge:
+      <NumericalInput
+        value={ ingredient.amount }
+        name='amount'
+        onChange={ (event): void => onChange(event, ingredient.name) }
+      />
+    </label>
+    <label>
+      <UnitInput
+        value={ ingredient.unit }
+        onChange={ (event): void => onChange(event, ingredient.name) }
+      />
+    </label>
+  </div>
+);
+
+export { IngredientFormPart };
